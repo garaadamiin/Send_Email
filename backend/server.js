a9c1052e04c202c16e190e4ed73f2f4b8023b6e9
garaad <garaadamiin3@gmail.com>
2022-11-24 14:08:01 +0300

    5 running

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on Port ${PORT}...`);
+});
